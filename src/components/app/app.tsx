e8715f54b495c8f882bc8737d482dfd186a80792
Bogdan Denisenko <denisenkob15@gmail.com>
2025-04-03 16:29:53 +0300

    Заменит Route на PrivateRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 import OfferScreen from '../../pages/offer-screen/offer-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   placeCardCount: number;
@@ -17,7 +18,7 @@ function App({placeCardCount}: AppScreenProps): JSX.Element {
         <Route path={AppRoute.Main}>
           <Route index element={<MainScreen placeCardCount={placeCardCount} />} />
           <Route path={AppRoute.Login} element={<LoginScreen />} />
-          <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
+          <Route path={AppRoute.Favorites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}><FavoritesScreen /></PrivateRoute>} />
           <Route path={AppRoute.Offer} element={<OfferScreen />}/>
           <Route path='*' element={<NotFoundScreen />} />
         </Route>
