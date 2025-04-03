60f18f66567071ae8424014aa6e3243dfe19d59b
Bogdan Denisenko <denisenkob15@gmail.com>
2025-04-03 16:13:07 +0300

    Добавит компонент приватного маршрута

--- src/const.ts
+++ src/const.ts
@@ -5,11 +5,18 @@ enum AppRoute {
   Offer = '/offer/:id'
 }
 
+enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
 const cities = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
 const placeCardCount = 5;
 
 export {
   AppRoute,
+  AuthorizationStatus,
   cities,
   placeCardCount
 };
