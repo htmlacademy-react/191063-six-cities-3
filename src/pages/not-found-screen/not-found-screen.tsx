f67159531eef900e47ebd37bce8305a35bb6e0f0
Bogdan Denisenko <denisenkob15@gmail.com>
2025-04-03 15:10:41 +0300

    Создаст компонент для страницы "404"

---
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,31 @@
+function NotFoundScreen() {
+  return (
+    <div className="page page--favorites-empty">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--favorites page__main--favorites-empty">
+        <div className="page__favorites-container container">
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Favorites (empty)</h1>
+            <div className="favorites__status-wrapper">
+              <b className="favorites__status">404 Not Found</b>
+              <a href="#">Return to Main Page</a>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
