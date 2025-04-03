072a9aed73a9af77b6c7824493d75f0c058c53d2
Bogdan Denisenko <denisenkob15@gmail.com>
2025-04-03 15:21:53 +0300

    Добавит константы для маршрутизации

--- src/const.ts
+++ src/const.ts
@@ -1,7 +1,15 @@
+enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id'
+}
+
 const cities = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
 const placeCardCount = 5;
 
 export {
+  AppRoute,
   cities,
-  placeCardCount,
+  placeCardCount
 };
