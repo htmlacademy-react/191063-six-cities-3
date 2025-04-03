031f0920a364dc3b4d7a80ce03c2967f66ac9d2d
Bogdan Denisenko <denisenkob15@gmail.com>
2025-04-03 15:47:09 +0300

    Добавит маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   placeCardCount: number;
@@ -6,7 +12,17 @@ type AppScreenProps = {
 
 function App({placeCardCount}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen placeCardCount={placeCardCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main}>
+          <Route index element={<MainScreen placeCardCount={placeCardCount} />} />
+          <Route path={AppRoute.Login} element={<LoginScreen />} />
+          <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
+          <Route path={AppRoute.Offer} element={<OfferScreen />}/>
+          <Route path='*' element={<NotFoundScreen />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
